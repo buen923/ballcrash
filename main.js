a284c099632dbe3583f75a3fb7a1a8222e25f0f1
buen923 <82753122+buen923@users.noreply.github.com>
2021-10-29 13:16:52 +0900

    Update main.js

--- main.js
+++ main.js
@@ -155,7 +155,7 @@ function spawnEnemies() {
         let y;
         let circledeg=0;
         let circlespeed=0.5;
-        let speed=Math.random()*3;
+        let speed=0.5
 
         const radius = Math.random() * (30 - 4) + 4;
 
