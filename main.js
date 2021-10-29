c4f88390e360169f0a4516d2893c59d13ed647cd
buen923 <82753122+buen923@users.noreply.github.com>
2021-10-29 11:05:57 +0900

    Update main.js

--- main.js
+++ main.js
@@ -264,7 +264,7 @@ function animate() {
     });
 }
 
-addEventListener('MSPointerDown', (event) => {
+c.addEventListener('click', (event) => {
     const angle = Math.atan2(event.clientY - canvas.height / 2,
         event.clientX - canvas.width / 2
     )
